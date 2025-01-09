f7d7df78a30651285b9cc87710e8b4b7e50cd8e7
Krochi <77779207+Krochi@users.noreply.github.com>
2025-01-09 14:05:03 +0300

    image_recognition.py

--- Diplom/my_diplom/app_project/image_recognition.py
+++ Diplom/my_diplom/app_project/image_recognition.py
@@ -1,3 +1,22 @@
+# Скрипт для распознавания изображений с помощью ResNet50 и библиотеки PyTorch
+"""
+Основные этапы:
+1. Загрузка предобученной модели ResNet50.
+2. Преобразование изображений.
+3. Прогнозирование класса изображения и вывод его вероятностей.
+4. Возвращение наименования класса, его индекса и вероятностей для всех классов.
+
+Скрипт включает несколько функций:
+--`predict_image`: Классификация одного изображения с выводом предсказанного класса.
+--`preprocess_image`: Предобработка изображения для подачи в модель.
+--`predict_image_probabilities`: Получение вероятности для всех классов на основе изображения.
+
+Зависимости:
+-- PyTorch
+-- torchvision
+-- PIL (Pillow)
+
+"""
 import torch
 from torchvision import models, transforms
 from torchvision.models import ResNet50_Weights
@@ -7,7 +26,7 @@ from PIL import Image as PILImage
 
 model = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
 model.eval()
-
+# Предобработка изображений
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -16,8 +35,16 @@ transform = transforms.Compose([
 ])
 
 imagenet_classes = ResNet50_Weights.IMAGENET1K_V1.meta["categories"]
-
+# Путь к изображению, классификация и возвращение навания и класса
 def predict_image(image_path):
+    """
+        Параметры:
+            image_path (str): Путь к изображению, которое необходимо классифицировать.
+
+        Возвращаем:
+            -- class_idx (int): Индекс предсказанного класса.
+            -- class_label (str): Название предсказанного класса.
+        """
     image = PILImage.open(image_path).convert("RGB")
     image_tensor = transform(image).unsqueeze(0)
     with torch.no_grad():
@@ -27,16 +54,30 @@ def predict_image(image_path):
     class_label = imagenet_classes[class_idx]
     return class_idx, class_label  # Возвращается кортеж
 
-
+# Загрузка и преобразование изображения перед передачей в ResNet50
 def preprocess_image(image_path):
+    """
+       Параметры:
+           image_path (str): Путь к изображению для предобработки.
+
+       Возвращаеv:
+           Tensor: Тензор изображения, готовый для подачи в модель.
+       """
     image = Image.open(image_path).convert('RGB')
     return transform(image).unsqueeze(0)
-
+ # Возвращение вероятности для всех классов в виде словаря, где ключи - это имена классов, а значение - вероятности для классов.
 def predict_image_probabilities(image_path):
+    """
+        Параметры:
+            image_path (str): Путь к изображению для анализа.
+
+        Возвращает:
+            predictions (dict).
+        """
     input_tensor = PILImage.open(image_path).convert("RGB")
     input_tensor = transform(input_tensor).unsqueeze(0)
 
-    with torch.no_grad():
+    with torch.no_grad(): # Предсказание и вычисление вероятности для всех классов
         outputs = model(input_tensor)
         probabilities = F.softmax(outputs, dim=1)
 
