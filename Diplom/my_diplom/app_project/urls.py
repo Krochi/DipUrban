4509d3b974ba3dc0f6643c2a866516f4aee5898b
Krochi <77779207+Krochi@users.noreply.github.com>
2025-01-09 14:25:43 +0300

    urls.py

--- Diplom/my_diplom/app_project/urls.py
+++ Diplom/my_diplom/app_project/urls.py
@@ -1,9 +1,34 @@
+# Определение маршрутов URL для приложения Django
+"""
+В этом файле указаны все маршруты, которые будут обрабатывать различные запросы к веб-приложению.
+
+Основные маршруты:
+-- Главная страница (`home`) - отображает домашнюю страницу приложения.
+-- Страница регистрации (`register`) - форма для регистрации нового пользователя.
+-- Страница дашборда (`dashboard`) - отображение информации о текущем пользователе.
+-- Страница загрузки изображения (`add_image_feed`) - форма для загрузки изображений.
+-- Страница удаления изображения (`delete_image`) - удаление изображения по ID.
+-- Страница логина (`login`) - форма для входа в систему с использованием стандартного механизма Django.
+-- Страница логаута (`logout`) - выход пользователя из системы с помощью стандартного механизма Django.
+-- Страница предсказаний вероятностей (`predict_probabilities`) - страница, на которой отображаются вероятности предсказания классов для изображений.
+
+Маршруты:
+Каждому маршруту соответствует своя функция или класс.
+
+Зависимости:
+-- `django.urls.path`: Для определения путей.
+-- `views`: Модуль с функциями представлений, обрабатывающими запросы.
+-- `django.contrib.auth.views`: Стандартные представления Django для входа и выхода из системы.
+-- `django.conf.settings`: Для работы с настройками Django.
+-- `django.conf.urls.static`: Для обслуживания статических и медиафайлов.
+"""
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+# "Path" функции из Django, с определённой функцией или классом представления. Список маршрутов:
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
@@ -15,6 +40,7 @@ urlpatterns = [
     path('predict_probabilities/', views.predict_probabilities, name='predict_probabilities'),
 
 ]
+# Обработка медиафайлов. Добавляет маршруты для медиафайлов
 static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
